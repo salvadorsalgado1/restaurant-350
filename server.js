61f2ef1dc7613ba086f65ce05cee1d3e0a1d1253
salvadorsalgado1 <salvadorsalgado5694@gmail.com>
2021-09-20 14:43:29 -0700

    test deploy to heroku

---
+++ server.js
@@ -0,0 +1,22 @@
+const express = require('express')
+const serverStatic = require('serve-static');
+const path = require('path')
+const cors = require('cors')
+const mysql = require('mysql')
+const {createPool} = require('mysql')
+const bodyParser = require('body-parser');
+const app = express();
+app.use(express.json())
+app.use(bodyParser.urlencoded({extended:true}))
+app.use(cors());
+
+if(process.env.NODE_ENV === 'production'){
+    //Set static folder
+    app.use(express.static(__dirname + '/'));
+    app.get(/^((?!(api)).)*$/, 
+    (req,res)=>res.sendFile(__dirname + '/index.html'))
+}
+const port = process.env.PORT || 5000;
+app.listen(port, ()=>{
+    console.log(`Currently running on port ${port}`)
+})
