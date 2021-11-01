beb9309e90f6240e23384f413ae1a25bd7a46e71
salvadorsalgado1 <salvadorsalgado5694@gmail.com>
2021-10-31 23:53:19 -0700

    add to Customer

--- src/views/Customer.vue
+++ src/views/Customer.vue
@@ -6,13 +6,16 @@
                   <div class="card">
                       <div class="card-body">
                           <h2>Current Reservations</h2>
+                            <ul class="list-group">
+                                <li class="list-group-item"> Reservation Data</li>
+                            </ul>
                       </div>
                   </div>
               </div>
               <div class="col-6">
                   <div class="card">
                       <div class="card-body">
-                          <h2>Past Reservations</h2>
+                          <h2>{{test}}</h2>
                       </div>
                   </div>
               </div>
@@ -23,7 +26,27 @@
 
 <script>
 export default {
+name:"Customer",
+    data(){
+        return{
+            
+        }
+    },
+    computed:{
+        currentUser(){
+            let reservations;
 
+
+
+
+            return reservations
+        },
+        test(){//Line 18 variable
+            
+            let message = 'Past Reservations'
+            return message;
+        }
+    }
 }
 </script>
 
